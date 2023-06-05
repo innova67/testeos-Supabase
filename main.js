c59cd3877f5bac9e6976b7633ff2610148c30361
Nova67 <innovagames75@gmail.com>
2023-06-05 02:35:28 -0400

    soy estupido y no lo guarde como string xd

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 // Create a single supabase client for interacting with your database
 const supabaseUrl = 'https://dngdzrclokvsobpfrxsa.supabase.co';
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZ2R6cmNsb2t2c29icGZyeHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5Mzg5NTUsImV4cCI6MjAwMTUxNDk1NX0.ZzPxaYrmuqknxknA63-tLLUdJzFazPUYDvm6Kv7b-mk;
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZ2R6cmNsb2t2c29icGZyeHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5Mzg5NTUsImV4cCI6MjAwMTUxNDk1NX0.ZzPxaYrmuqknxknA63-tLLUdJzFazPUYDvm6Kv7b-mk";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // get html div id
