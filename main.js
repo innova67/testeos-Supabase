184c5c285a7fb19458ef851b232fd0806a5e64e7
Nova67 <innovagames75@gmail.com>
2023-06-05 02:05:09 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabaseUrl = 'https://dngdzrclokvsobpfrxsa.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+// get html div id
+const visDatos = document.getElementById("clientes");
+
+
+function cargarDB() {
+    // tener los datos a la mano
+    const { data, error } = await supabase
+        .from('clientes')
+        .select('nombres')
+    
+    //revisar que todo esté bien
+    console.log(data);
+    console.log(error);
+}
