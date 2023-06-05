6275e1d6080b4c0091ebc3f7fba6f8d5da3ff5bc
Nova67 <innovagames75@gmail.com>
2023-06-05 02:40:10 -0400

    crear acceso de cliente y mod una var

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // Create a single supabase client for interacting with your database
 const supabaseUrl = 'https://dngdzrclokvsobpfrxsa.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZ2R6cmNsb2t2c29icGZyeHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU5Mzg5NTUsImV4cCI6MjAwMTUxNDk1NX0.ZzPxaYrmuqknxknA63-tLLUdJzFazPUYDvm6Kv7b-mk";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const db = supabase.createClient(supabaseUrl, supabaseKey);
 
 // get html div id
 const visDatos = document.getElementById("clientes");
@@ -12,7 +12,7 @@ const visDatos = document.getElementById("clientes");
 
 async function cargarDB() {
     // tener los datos a la mano
-    const { data, error } = await supabase
+    const { data, error } = await db
         .from('clientes')
         .select('nombres')
     
